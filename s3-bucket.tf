06477adaf173292d111648f4663855a173dec0c6
Axel Thiebaut <axel.thiebaut@devoteam.com>
2023-10-05 15:58:17 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/axel-thbt/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
+  bucket = "my-fefefefe-bucket"
   acl    = "private"
 
   versioning = {
